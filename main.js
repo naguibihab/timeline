e469e160737fd9b1eb8fc357863aed20e2ee7b4a
Naguib <nick@perceptivemind.com.au>
2017-11-05 15:57:50 +1100

    Modified the popoup to connect to javascript

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // Function used for adding tasks to timeline
 function addTask(){
+	var date = document.getElementById('date');
+	var time = document.getElementById('time');
+	var title = document.getElementById('title');
+	var desc = document.getElementById('desc');
 
+	console.log(date,time,title,desc);
 }
