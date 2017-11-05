2255e0113f7394e6882b18aea3020167eec87a4f
Naguib <nick@perceptivemind.com.au>
2017-11-05 18:38:55 +1100

    Broke down JS into two functions to loop the second function

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# Bugs & issues
+- Title doesn't go anywhere, it should replace the type
+
+# Tasks 1
+- Scroll to zoom
+- Drag to move
+
+# Tasks 2
+- Hover state on buttons & dots
+- Header icons
--- main.js
+++ main.js
@@ -13,41 +13,48 @@ function addTask(){
 	var types = ['Appointment','Meeting'];
 
 	// Caputre inputs
+	var taskData = [];
 	var dateDOM = document.getElementById('date');
 	var date = new Date(dateDOM.value);
-	var dayNumber = date.getUTCDate() < 10 ? '0'+date.getUTCDate() : date.getUTCDate();
-	var dayName = days[date.getDay()];
-	var month = months[date.getUTCMonth()];
-	var year = date.getUTCFullYear();
+	taskData.dayNumber = date.getUTCDate() < 10 ? '0'+date.getUTCDate() : date.getUTCDate();
+	taskData.dayName = days[date.getDay()];
+	taskData.month = months[date.getUTCMonth()];
+	taskData.year = date.getUTCFullYear();
 
-	var author = 'John Doe'; // For now this is hardcoded
+	taskData.author = 'John Doe'; // For now this is hardcoded
 
 	var timeDOM = document.getElementById('time');
+	taskData.time = timeDOM.value;
 	var titleDOM = document.getElementById('title');
+	taskData.title = titleDOM.value;
 	var typeDOM = document.getElementById('type');
-	var type = types[typeDOM.value];
+	taskData.type = types[typeDOM.value];
 	var descDOM = document.getElementById('desc');
+	taskData.desc = descDOM.value;
 
+	
+
+	addTaskHtml(taskData);
+}
+
+function addTaskHtml(taskData){
 	// Count li elements to see if we're adding an odd or an even li
 	var oddElement = document.getElementsByClassName('timeline-li').length %2 == 0;
-
+	
 	// Add HTML
 	var timelineElement = document.getElementById('timeline-ul');
-
 	var newTaskElement = `<li class="timeline-li">`;
-
-
 	// The only part that's different between odd and even tasks is the order of the
 	// .icon elements and the direction of the margin
 	if(oddElement){
 		newTaskElement += `
 				<div class="icon blue">
-					<div class="event-type inline mr-5">`+type+`</div>
+					<div class="event-type inline mr-5">`+taskData.type+`</div>
 					<div class="container inline">
 			    		<span class="event-icon">
 			    			<i class="fa fa-calendar" aria-hidden="true"></i>
 			    		</span>
-			    		<span class="event-time block grey">`+timeDOM.value+`</span>
+			    		<span class="event-time block grey">`+taskData.time+`</span>
 			    	</div>
 				</div>`;
 	} else {
@@ -57,30 +64,30 @@ function addTask(){
 			    		<span class="event-icon">
 			    			<i class="fa fa-calendar" aria-hidden="true"></i>
 			    		</span>
-			    		<span class="event-time block grey">`+timeDOM.value+`</span>
+			    		<span class="event-time block grey">`+taskData.time+`</span>
 			    	</div>
-					<div class="event-type inline ml-5">`+type+`</div>
+					<div class="event-type inline ml-5">`+taskData.type+`</div>
 				</div>`;
 	}
 
 	newTaskElement += `
 				<div class="content">
 					<div class="event-date">
-						<span class="day-number">`+dayNumber+`</span>
+						<span class="day-number">`+taskData.dayNumber+`</span>
 						<div class="container inline ml-10">
-							<span class="day-name block">`+dayName+`</span>
-							<span class="month-year block grey">`+month+' '+year+`</span>
+							<span class="day-name block">`+taskData.dayName+`</span>
+							<span class="month-year block grey">`+taskData.month+' '+taskData.year+`</span>
 						</div>
 					</div>
 					<div class="event-type grey"><i class="fa fa-info-circle" aria-hidden="true"></i>`+type+`</div>
 					<div class="event-text grey">
 						<i class="fa fa-pencil" aria-hidden="true"></i>
 						<p class="inline">
-							`+descDOM.value+`
+							`+taskData.desc+`
 						</p>
 					</div>
 				</div>
-				<div class="author grey">by `+author+`</div>
+				<div class="author grey">by `+taskData.author+`</div>
 			</li>`
 
 	timelineElement.innerHTML += newTaskElement;
